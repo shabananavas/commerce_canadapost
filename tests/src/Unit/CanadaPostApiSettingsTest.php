bb12ce7c2b7c73630ea987e061e2a758e820c1e3
Shabana Navas <snavas@acromedia.com>
2018-12-17 22:33:42 +0530

    Add test to make sure shipping method gets preference.

--- tests/src/Unit/CanadaPostApiSettingsTest.php
+++ tests/src/Unit/CanadaPostApiSettingsTest.php
@@ -16,7 +16,7 @@ class CanadaPostApiSettingsTest extends CanadaPostUnitTestBase {
    * ::covers getRequestConfig.
    */
   public function testShippingMethodApiSettingsReturned() {
-    // Set the API settings w/o passing a store entity.
+    // Get the API settings w/o passing a store entity.
     $utilities = new UtilitiesService();
     $api_settings = $utilities->getApiSettings(NULL, $this->shippingMethod);
 
@@ -31,7 +31,7 @@ class CanadaPostApiSettingsTest extends CanadaPostUnitTestBase {
    * ::covers getRequestConfig.
    */
   public function testStoreApiSettingsReturned() {
-    // Set the API settings passing a store entity.
+    // Get the API settings passing a store entity.
     $utilities = new UtilitiesService();
     $api_settings = $utilities->getApiSettings($this->shipment->getOrder()->getStore());
 
@@ -40,6 +40,23 @@ class CanadaPostApiSettingsTest extends CanadaPostUnitTestBase {
     $this->assertEquals('store_mock_name', $api_settings['username']);
     $this->assertEquals('store_mock_pwd', $api_settings['password']);
     $this->assertEquals('live', $api_settings['mode']);
+  }
+
+  /**
+   * ::covers getRequestConfig.
+   */
+  public function testShippingMethodHasPreference() {
+    // Get the API settings passing a store entity and shipping method entity
+    // and test that we get back the shipping method settings as that has
+    // preference.
+    $utilities = new UtilitiesService();
+    $api_settings = $utilities->getApiSettings($this->shipment->getOrder()->getStore(), $this->shippingMethod);
+
+    // Now, test that we are returned back the store API settings.
+    $this->assertEquals('shipment_method_mock_cn', $api_settings['customer_number']);
+    $this->assertEquals('shipment_method_mock_name', $api_settings['username']);
+    $this->assertEquals('shipment_method_mock_pwd', $api_settings['password']);
+    $this->assertEquals('test', $api_settings['mode']);
   }
 
 }
